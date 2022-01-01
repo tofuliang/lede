8fcacf69de89e12d8e806808a76bdaa052bb81d5
tofuliang <tofuliang@gmail.com>
2022-01-01 23:17:07 +0800

    Update prebuild.sh

--- prebuild.sh
+++ prebuild.sh
@@ -2,7 +2,7 @@ export ReleaseVersion="$(git rev-parse --short HEAD^)-$(date +%Y.%m.%d)"
 
 sed -i "/helloworld/d" "feeds.conf.default"
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
-./scripts/feeds update -a
+./scripts/feeds update -a -f
 ./scripts/feeds install -a -f -p helloworld
 
 rm -fr luci-compat-app-smartdns
